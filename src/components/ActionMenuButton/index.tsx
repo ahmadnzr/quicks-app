5411132f84a8da573872b73491aa5b9fa0697fd3
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2024-08-28 19:55:31 +0700

    fix(ui): custom width, weight

--- src/components/ActionMenuButton/index.tsx
+++ src/components/ActionMenuButton/index.tsx
@@ -1,16 +1,23 @@
 import { Popover } from "antd";
+import { TooltipPlacement } from "antd/es/tooltip";
 import styled from "styled-components";
+
+import { FontWeight } from "../../helpers/types";
+import { Colors } from "../../helpers/utils";
 
 export interface MenuType {
   key: number;
   label: string;
   color?: string;
+  weight?: FontWeight;
 }
 
 export interface ActionMenuProps {
   menus: MenuType[];
   onClickMenu: (menu: MenuType) => void;
   className?: string;
+  customWidth?: string;
+  placement?: TooltipPlacement;
   children?: React.ReactNode;
 }
 
@@ -18,7 +25,9 @@ export const ActionMenuButton = ({
   children,
   onClickMenu,
   className,
+  placement = "bottomLeft",
   menus = [],
+  customWidth,
 }: ActionMenuProps) => {
   const handleClickMenu = (menu: MenuType) => {
     onClickMenu(menu);
@@ -27,15 +36,16 @@ export const ActionMenuButton = ({
   return (
     <Popover
       trigger="click"
-      placement="bottomLeft"
+      placement={placement}
       title=""
       arrow={false}
       content={
-        <ActionMenuButtonStyled className={className}>
+        <ActionMenuButtonStyled className={className} $width={customWidth}>
           {menus.map((item) => (
             <ActionItem
               key={item.key}
-              $color={item.color}
+              $color={item.color || Colors.primary.grayDark}
+              $weight={item.weight || "regular"}
               onClick={() => handleClickMenu(item)}
             >
               {item.label}
@@ -49,8 +59,8 @@ export const ActionMenuButton = ({
   );
 };
 
-const ActionMenuButtonStyled = styled.div`
-  width: 126px;
+const ActionMenuButtonStyled = styled.div<{ $width?: string }>`
+  width: ${({ $width = "126px" }) => $width};
   border: 1px solid #bdbdbd;
   background-color: #fff;
   border-radius: 5px;
@@ -60,7 +70,7 @@ const ActionMenuButtonStyled = styled.div`
   }
 `;
 
-const ActionItem = styled.button<{ $color?: string }>`
+const ActionItem = styled.button<{ $color?: string; $weight: FontWeight }>`
   width: 100%;
   padding: 14px 18px;
   text-align: left;
@@ -68,6 +78,7 @@ const ActionItem = styled.button<{ $color?: string }>`
   background-color: transparent;
   color: ${(props) => props.$color};
   cursor: pointer;
+  font-weight: ${(props) => props.theme.fontWeight[props.$weight]};
 
   &:active {
     background: rgba(130, 130, 130, 0.1);
